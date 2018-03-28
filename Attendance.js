30e255a9d8861b9db0ade44032a4a5beeab84232
cstone001 <cstone2191@gmail.com>
2018-03-28 14:28:20 -0400

    Update Attendance.js
    Added click events and warnings

--- Attendance.js
+++ Attendance.js
@@ -1,10 +1,26 @@
+var myarr = [];
+var size = 5;
+
+function addTo() {
+   myarr = myarr.filter(Boolean); //DOESN'T STORE EMPTY ARRAY VALUES
+   myarr.push(document.getElementById("Input").value);
+   console.log(myarr);
+}
+
 $('#new').click( function() {
    var input = $('#Input').val();
+   
   if($("#Input").val() == '') {
     $('#alert').html("<strong>Warning!</strong> You didn't enter a name");
     $('#alert').fadeIn().delay(1500).fadeOut();
 
-    return false
+    return false;
+  } else if (myarr.length > size) {
+    $('#alert').html("<strong>Warning!</strong> Your class is full");
+    $('#alert').fadeIn().delay(2500).fadeOut();
+
+    $("#Input").hide()
+    $("#new").hide()
   }
    $("#students").append('<li>' + input + '</li>');
 
@@ -30,4 +46,12 @@ list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('present');
   }
+  //ON DOUBLE CLICK REMOVE STUDENT'S NAME AND SLIDE UP
+  $(document).ready(function(){
+      $("li").dblclick(function(){
+          $(this).slideUp('slow', function (){
+            $(this).remove();
+          });
+      });
+  }); 
 });
